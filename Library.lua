f8645d1f00e0a90628fa051b81f180af426472ec
BigHacker123 <116747999+BigHacker123@users.noreply.github.com>
2022-12-28 10:07:53 +0000

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -858,7 +858,7 @@ do
                 end;
             end;
 
-            Library.KeybindFrame.Size = UDim2.new(0, math.max(XSize + 10, 210), 0, YSize + 23)
+            Library.KeybindFrame.Size = UDim2.new(0, math.max(XSize + 10, 175), 0, YSize + 23)
         end;
 
         function KeyPicker:GetState()
